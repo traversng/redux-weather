c35b61e55934190c7d1a5e0e20d7e9c53eb050a4
travis ueki <travis ueki>
2016-04-17 21:37:07 -0700

    working search with valid response from weather api

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from '../containers/search-bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <SearchBar />
     );
   }
 }
